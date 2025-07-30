245ff1ca1f1040f48cd82361efbeb3b255b535cd
inv-athul-krishna-s <athul.s@innovaturelabs.com>
2025-07-30 11:11:19 +0530

    updated login  page

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login (Bootstrap)</title>
+    <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light d-flex justify-content-center align-items-center vh-100">
